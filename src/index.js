f489a0bb22024c5f232e8366b484d18b7bb50b6c
qrvck <v.kapitan2010@gmail.com>
2022-05-08 01:41:07 +0300

    feat: add generation of DOM elements using js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// sryles
+import "./scss/styles.scss";
+
+// scripts
+import "./scripts/createHtmlMarkup";
---
+++ src/scripts/createHtmlMarkup.js
@@ -0,0 +1,92 @@
+let body = document.querySelector("body");
+let dataOfBtns = [
+  [ ["Backquote", [["Ё","Ё","ё"], ["`","~","`"]]], ["Digit1", [["1","!","1"], ["1","!","1"]]], ["Digit2", [["2","&quot;","2"], ["2","@","2"]]], ["Digit3", [["3","№","3"], ["3","#","3"]]], ["Digit4", [["4",";","4"], ["4","$","4"]]], ["Digit5", [["5","%","5"], ["5","%","5"]]], ["Digit6", [["6",":","6"], ["6","^","6"]]], ["Digit7", [["7","?","7"], ["7","&","7"]]], ["Digit8", [["8","*","8"], ["8","*","8"]]], ["Digit9", [["9","(","9"], ["9","(","9"]]], ["Digit0", [["0",")","0"], ["0",")","0"]]], ["NumpadSubtract", [["-","_","-"], ["-","_","-"]]], ["Equal", [["=","+","="], ["=","+","="]]], ["Backspace", ["Backspace"]] ],
+  [ ["Tab", ["Tab"]], ["KeyQ", [["Й","Й","й"], ["Q","Q","q"]]], [ "KeyW", [["Ц","Ц","ц"], ["W","W","w"]]], [ "KeyE", [["У","У","у"], ["E","E","e"]]], ["KeyR", [["К","К","к"], ["R","R","r"]]], ["KeyT", [["Е","Е","е"], ["T","T","t"]]], ["KeyY", [["Н","Н","н"], ["Y","Y","y"]]], ["KeyU", [["Г","Г","г"], ["U","U","u"]]], ["KeyI", [["Ш","Ш","ш"], ["I","I","i"]]], ["KeyO", [["Щ","Щ","щ"], ["O","O","o"]]], ["KeyP", [["З","З","з"], ["P","P","p"]]], ["BracketLeft", [["Х","Х","х"], ["[","{","["]]], ["BracketRight", [["Ъ","Ъ","ъ"], ["]","}","]"]]], ["Backslash", [["&#92;","/","&#92;"], ["&#92;","|","&#92;"]]], ["NumpadDecimal", ["Del"]] ],
+  [ ["CapsLock", ["CapsLk"]], ["KeyA", [["Ф","Ф","ф"], ["A","A","a"]]], ["KeyS", [["Ы","Ы","ы"], ["S","S","s"]]], ["KeyD", [["В","В","в"], ["D","D","d"]]], ["KeyF", [["А","А","а"], ["F","F","f"]]], ["KeyG", [["П","П","п"], ["G","G","g"]]], ["KeyH", [["Р","Р","р"], ["H","H","h"]]], ["KeyJ", [["О","О","о"], ["J","J","j"]]], ["KeyK", [["Л","Л","л"], ["K","K","k"]]], ["KeyL", [["Д","Д","д"], ["L","L","l"]]], ["Semicolon", [["Ж","Ж","ж"], [";",":",";"]]], ["Quote", [["Э","Э","э"], ["'","&#34;","'"]]], ["Enter", ["Enter"]] ],
+  [ ["ShiftLeft", ["Shift"]], ["KeyZ", [["Я","Я","я"], ["Z","Z","z"]]], ["KeyX", [["Ч","Ч","ч"], ["X","X","x"]]], ["KeyC", [["С","С","с"], ["C","C","c"]]], ["KeyV", [["М","М","м"], ["V","V","v"]]], ["KeyB", [["И","И","и"], ["B","B","b"]]], ["KeyN", [["Т","Т","т"], ["N","N","n"]]], ["KeyM", [["Ь","Ь","ь"], ["M","M","m"]]], ["NumpadDecimal", [["Б","Б","б"], [",","<",","]]], ["Period", [["Ю","Ю","ю"], [".",">","."]]], ["Slash", [[".",",","."], ["/","?","/"]]], ["ArrowUp", ["▲"]], ["ShiftRight", ["Shift"]] ],
+  [ ["ControlLeft", ["Ctrl"]], ["MetaLeft", ["Win"]], ["AltLeft", ["Alt"]], ["Space", [" "]], ["AltRight", ["Alt"]], ["ArrowLeft", ["◄"]], ["ArrowDown", ["▼"]], ["ArrowRight", ["►"]], ["ControlRight", ["Ctrl"]] ]
+];
+
+
+function createTextarea() {
+  let textarea = document.createElement("textarea");
+  textarea.classList.add("output");
+  textarea.id = "output";
+  return textarea;
+}
+
+
+function createRows(amount) {
+  let rows = [];
+  
+  for (let i = 0; i < amount; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    rows.push(row);
+  }
+  
+  return rows;
+}
+
+
+function createBtn(btn) {
+  let key = document.createElement("div");
+  let [id, data] = btn;
+  
+  
+  if (data.length == 2) {
+    key.classList.add("key");
+    key.id = id;
+    
+    let langClassis = ["ru", "en"];
+    let spanClassis = ["capslk", "shift", "lowercase"];
+
+    for (let i = 0; i < langClassis.length; i++) {
+      let div = document.createElement("div");
+      div.classList.add(langClassis[i]);
+  
+      for (let j = 0; j < spanClassis.length; j++) {
+        let span = document.createElement("span");
+        span.classList.add(spanClassis[j]);
+        span.innerHTML = data[i][j];
+        div.append(span);
+      }
+  
+      key.append(div);
+    }
+  } else {
+    key.classList.add("func-key");
+    key.classList.add(`func-key--${id.toLowerCase()}`);
+    key.id = id;
+    key.innerHTML = data[0];
+  }
+
+  return key;
+}
+
+
+function createKeyboard(dataOfBtns) {
+  let keyboard = document.createElement("div");
+  keyboard.classList.add("keyboard");
+  keyboard.classList.add("russian");
+
+
+  let rows = createRows(dataOfBtns.length);
+
+  for (let i = 0; i < rows.length; i++) {
+    
+    for (let j = 0; j < dataOfBtns[i].length; j++) {
+      let btn = createBtn(dataOfBtns[i][j]);
+      rows[i].append(btn);
+    }
+
+  }
+
+  keyboard.append(...rows);
+  return keyboard;
+}
+
+let textarea = createTextarea();
+let keyboard = createKeyboard(dataOfBtns);
+body.append(textarea);
+body.append(keyboard);
