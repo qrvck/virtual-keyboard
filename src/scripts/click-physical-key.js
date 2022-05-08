085cc4e5f8a55bb7f9058caa317f06c2e364d3c1
qrvck <v.kapitan2010@gmail.com>
2022-05-08 22:54:02 +0300

    feat: add action capslock and shift when physical keys

--- src/scripts/click-physical-key.js
+++ src/scripts/click-physical-key.js
@@ -7,8 +7,19 @@ document.addEventListener("keydown", (evt) => {
   evt.preventDefault();
   let keyCode = evt.code;
   let key = keyboard.querySelector(`#${keyCode}`);
+
+  if (keyboard.classList.contains("on-capslock") && (keyCode === "ShiftLeft" || keyCode === "ShiftRight")) {
+    return;
+  } else if (keyboard.classList.contains("on-shift") && keyCode === "CapsLock") {
+    return;
+  }
+
   if (keyCode === "CapsLock") {
     key.classList.toggle("active");
+    keyboard.classList.toggle("on-capslock");
+  } else if (keyCode === "ShiftLeft" || keyCode === "ShiftRight") {
+    keyboard.classList.add("on-shift");
+    key.classList.add("active");
   } else {
     key.classList.add("active");
   }
@@ -21,14 +32,10 @@ document.addEventListener("keydown", (evt) => {
 });
 
 
-
-
-
-
-
 document.addEventListener("keyup", (evt) => {
   let keyCode = evt.code;
   if (keyCode === "CapsLock") return;
+  if (keyCode === "ShiftLeft" || keyCode === "ShiftRight") keyboard.classList.remove("on-shift");
   let key = keyboard.querySelector(`#${keyCode}`);
   key.classList.remove("active");
 
