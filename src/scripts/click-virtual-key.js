68aa432bf54b5fe808a8b0fd2dbaafd213e20db1
qrvck <v.kapitan2010@gmail.com>
2022-05-09 06:28:36 +0300

    feat: do working shift and capsLk on virtual keyboard

--- src/scripts/click-virtual-key.js
+++ src/scripts/click-virtual-key.js
@@ -1,10 +1,18 @@
 import {outputKey} from "./output-key";
+import { addBacklight, removeBacklight } from "./add-backlight-key";
 
 let keyboard = document.querySelector(".keyboard");
 let output = document.querySelector(".output");
 
-keyboard.addEventListener("click", (evt) => {
+keyboard.addEventListener("mousedown", (evt) => {
   let key = evt.target.closest(".key");
 
   outputKey(key, output);
+  addBacklight(key.id, key, keyboard);
+});
+
+keyboard.addEventListener("mouseup", (evt) => {
+  let key = evt.target.closest(".key");
+
+  removeBacklight(key.id, keyboard);
 });
